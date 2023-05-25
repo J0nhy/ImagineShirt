4e27275cd8ff4a51a5561b782ebb888c5bc15d52
João Custódio <jpsasilva@hotmail.com>
2023-05-25 15:40:31 +0100

    ajustes

--- resources/views/catalogo/show.blade.php
+++ resources/views/catalogo/show.blade.php
@@ -10,8 +10,8 @@
 @endsection
 
 @section('main')
-    <section class="sec-product-detail bg0 p-t-65 p-b-60 ">
-        <div class="container">
+    <section class="sec-product-detail bg0  p-t-65 p-b-60 ">
+        <div class="container bg0 p-t-65">
             <div class="row">
                 <div class="col-md-6" style="position:relative;">
 
@@ -19,15 +19,15 @@
                     <img id="baseTshirt"src="/tshirt_base/00a2f2.jpg" alt="IMG-PRODUCT"
                         style="width: 80%; height: 80%;   max-height: none;object-fit: contain; position:absolute; z-index: 1;">
                     <div style="height: 250px;
-                    width: 200px;  position:absolute; z-index: 3; top: 35%; left: 45%; transform: translate(-60%,-50%);">
+                    width: 200px;  position:absolute; z-index: 3; top: 40%; left: 45%; transform: translate(-52%,-50%);">
 
-                        <img id="fotoTshirt"src="/tshirt_images/{{ $tshirt->image_url }}" alt="IMG-PRODUCT" style="object-fit: cover; max-width:200px; max-height:350px;">
+                        <img id="fotoTshirt"src="/tshirt_images/{{ $tshirt->image_url }}" alt="IMG-PRODUCT" style="object-fit: cover; max-width:160px; max-height:350px;">
                     </div>
 
 
                 </div>
 
-                <div class="col-md-6 col-lg-5 p-b-30">
+                <div class="col-md-6 col-lg-5 p-b-30 ">
                     <div class="p-r-50 p-t-5 p-lr-0-lg">
                         <h4 class="mtext-105 cl2 js-name-detail p-b-14">
                             {{ $tshirt->name }}
@@ -49,7 +49,7 @@
                                 </div>
 
                                 <div class="size-204 respon6-next">
-                                    <div class="rs1-select2 bor8 bg0">
+                                    <div class="rs1-select2 bor8 ">
                                         <select class="js-select2" name="time">
                                             <option>Tamanho</option>
                                             <option>S</option>
@@ -73,9 +73,9 @@
 
                             </div>
 
-                            <div class="flex-w flex-r-m p-b-10">
-                                <div class="size-204 flex-w flex-m respon6-next">
-                                    <div class="wrap-num-product flex-w m-r-20 m-tb-10">
+                            <div class="flex-w flex-c-str p-b-10">
+                                <div class=" flex-w flex-m respon6-next">
+                                    <div class="wrap-num-product flex-w  m-r-20 m-tb-10">
                                         <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
                                             <i class="fs-16 zmdi zmdi-minus"></i>
                                         </div>
@@ -86,40 +86,15 @@
                                         <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
                                             <i class="fs-16 zmdi zmdi-plus"></i>
                                         </div>
-                                    </div>
 
+                                    </div>
                                     <button
                                         class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail">
                                         Adicionar ao carrinho
-                                    </button>
+                                        </button>
+
                                 </div>
                             </div>
-                        </div>
-
-                        <!--  -->
-                        <div class="flex-w flex-m p-l-100 p-t-40 respon7">
-                            <div class="flex-m bor9 p-r-10 m-r-11">
-                                <a href="#"
-                                    class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 js-addwish-detail tooltip100"
-                                    data-tooltip="Add to Wishlist">
-                                    <i class="zmdi zmdi-favorite"></i>
-                                </a>
-                            </div>
-
-                            <a href="#" class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 m-r-8 tooltip100"
-                                data-tooltip="Facebook">
-                                <i class="fa fa-facebook"></i>
-                            </a>
-
-                            <a href="#" class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 m-r-8 tooltip100"
-                                data-tooltip="Twitter">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-
-                            <a href="#" class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 m-r-8 tooltip100"
-                                data-tooltip="Google Plus">
-                                <i class="fa fa-google-plus"></i>
-                            </a>
                         </div>
                     </div>
                 </div>
