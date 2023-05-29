b89fbc1484082562a019129c528018110e6ffed4
João Custódio <jpsasilva@hotmail.com>
2023-05-29 13:22:28 +0100

    home do carrinho a ir para a pagina principal

--- resources/views/carrinho/cart.blade.php
+++ resources/views/carrinho/cart.blade.php
@@ -58,7 +58,7 @@ $iterator=0;
 	<!-- breadcrumb -->
 	<div class="container">
 		<div class="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
-			<a href="index.html" class="stext-109 cl8 hov-cl1 trans-04">
+			<a href="/" class="stext-109 cl8 hov-cl1 trans-04">
 				Home
 				<i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
 			</a>
@@ -112,7 +112,7 @@ $iterator=0;
 										<td id="total<?= $iterator; ?>" class="column-5"><?= $price[0]->unit_price_catalog; ?>€</td>
 										<td class="column-6"><a class="linkBranco" href="/removeFromCart/{{$item->image_url}};<?= $cores[$iterator]; ?>;<?= $sizes[$iterator]; ?>"><i class="zmdi zmdi-close iconBigger"></i></a></td>
 								</tr>
-								<?php 
+								<?php
 									$iterator++;
 								?>
 								@endforeach
@@ -388,7 +388,7 @@ function changeTotal($id)
     total.innerHTML = totalVal + ".00€";
 }
 function changeQty($op, $id)
-{	
+{
 	var qty = document.querySelector('#qty' + $id);
 	if($op == "-"){
 		if(qty.value > 1){
